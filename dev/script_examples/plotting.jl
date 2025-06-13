2589fdb1a62d0a81ceca63b3e0740043eaca84c5
Documenter.jl <documenter@juliadocs.github.io>
2025-06-13 19:27:46 +0000

    build based on 471faf8

--- dev/script_examples/plotting.jl
+++ dev/script_examples/plotting.jl
@@ -15,6 +15,8 @@
 
 using ExtendableGrids
 using GridVisualize
+using Triangulate: Triangulate, TriangulateIO, triangulate
+using Printf
 
 # ### 1D grids
 function grid1d(; n = 50)
@@ -397,6 +399,39 @@ end
 
 # ![](plotting_custom.png)
 
+# ## TriangulateIO
+#
+# GridVisualize comes with [`GridVisualize.plot_triangulateio`](@ref), a method to plot the input/output
+# struct of the Triangle mesh generator, provided by the [Triangulate.jl](https://github.com/JuliaGeometry/Triangulate.jl) wrapper.
+# Supported are PyPlot and Makie backends.
+#
+#
+function plotting_triangulateio(; Plotter = default_plotter(), maxarea = 0.05, resolution = (600, 300))
+    triin = Triangulate.TriangulateIO()
+    triin.pointlist = Matrix{Cdouble}([0.0 0.0; 1.0 0.0; 1.0 1.0; 0.6 0.6; 0.0 1.0]')
+    triin.segmentlist = Matrix{Cint}([1 2; 2 3; 3 4; 4 5; 5 1]')
+    triin.segmentmarkerlist = Vector{Int32}([1, 2, 3, 4, 5])
+    area = @sprintf("%.15f", maxarea)
+    (triout, vorout) = triangulate("pa$(area)DQ", triin)
+
+    vis = GridVisualizer(;
+        Plotter, layout = (1, 2), clear = true, resolution
+    )
+
+    plot_triangulateio!(vis[1, 1], triin, title = "input")
+    plot_triangulateio!(
+        vis[1, 2],
+        triout;
+        voronoi = vorout,
+        circumcircles = true,
+        title = "output"
+    )
+    return reveal(vis)
+end;
+
+# ![](plotting_triangulateio.png)
+
+
 plotting_functions_png = [
     :plotting_multiscene,
     :plotting_func1d,
@@ -412,6 +447,7 @@ plotting_functions_png = [
     :plotting_grid2d,
     :plotting_grid3d,
     :plotting_custom,
+    :plotting_triangulateio,
 ]
 
 plotting_functions_gif = [
